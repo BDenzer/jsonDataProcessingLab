e573a86d3ba48185c0f11119d3af25ed9b1b05a6
BDenzer <denz0045@morris.umn.edu>
2015-02-12 13:10:37 -0600

    made specified changes from website. started to add some testing.

--- client/app/dob/dob.controller.spec.js
+++ client/app/dob/dob.controller.spec.js
@@ -1,3 +1,32 @@
-/**
- * Created by denz0045 on 2/10/15.
- */
+'use strict';
+
+describe('Controller: dobCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('jsonDataProcessingLabApp'));
+  beforeEach(module('socketMock'));
+
+  var dobCtrl,
+    scope,
+    $httpBackend;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
+    $httpBackend = _$httpBackend_;
+    $httpBackend.expectGET('/api/student')
+      .respond(['HTML5 Boilerplate', 'AngularJS', 'Karma', 'Express']);
+
+    scope = $rootScope.$new();
+    dobCtrl = $controller('dobCtrl', {
+      $scope: scope
+    });
+  }));
+
+  it('should pass', function(){
+    expect(true).toEqual(true);
+  })
+  it('should return a DOB', function(){
+    $httpBackend.post('api/student',{dateOfBirth: 10});
+    expect(10).toEqual(scope.sortByDob())
+  })
+});
--- client/app/dob/dob.js
+++ client/app/dob/dob.js
@@ -1,3 +1,14 @@
 /**
  * Created by denz0045 on 2/10/15.
  */
+'use strict';
+
+angular.module('jsonDataProcessingLabApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('dob', {
+        url: '/',
+        templateUrl: 'app/dob/dob.html',
+        controller: 'dobCtrl'
+      });
+  });
