7e8e9f87079c1def9fb057f233fd6f04bdf4fadd
ingraval <ingr0071@morris.umn.edu>
2015-02-19 13:39:54 -0600

    Added new files to git

---
+++ client/app/sortByMajor/sortByMajor.controller.js
@@ -0,0 +1,40 @@
+'use strict';
+
+angular.module('jsonDataProcessingLabApp')
+  .controller('SortByMajorCtrl', function ($scope) {
+    $scope.message = 'Hello';
+
+    $scope.studentsInfo = [];
+
+    $http.get('/api/students').success(function(studentsInfo) {
+      $scope.studentsInfo = studentsInfo;
+
+      socket.syncUpdates('student', $scope.studentsInfo);
+
+    });
+
+    $scope.$on('$destroy', function () {
+      socket.unsyncUpdates('student');
+    });
+
+    $scope.myVar = true;
+    $scope.myVar2 = true;
+
+    $scope.displayLastNames = function() {
+      $scope.myVar = !$scope.myVar;
+      if ($scope.myVar2 == false) {
+        $scope.myVar2 = !$scope.myVar2;
+      }
+
+    };
+
+    $scope.displayFirstNames = function() {
+      $scope.myVar2 = !$scope.myVar2;
+      if ($scope.myVar == false) {
+        $scope.myVar = !$scope.myVar;
+      }
+    };
+
+
+  });
+
