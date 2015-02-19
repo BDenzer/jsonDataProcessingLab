97e4503f51a6115cd6301fe85b77dac34ec87003
BDenzer <denz0045@morris.umn.edu>
2015-02-19 13:30:37 -0500

    got DOB sort working

--- client/app/dob/dob.controller.js
+++ client/app/dob/dob.controller.js
@@ -10,16 +10,5 @@ angular.module('jsonDataProcessingLabApp')
         $scope.studentsInfo = studentsInfo;
         socket.syncUpdates('student', $scope.studentsInfo);
     });
-    $scope.sortByDob = function(){
-        var dob = [];
-        for (var i = 0; i < $scope.studentsInfo.length; i++){
-            dob.push(($scope.studentsInfo[i].dateOfBirth))
-        }
 
-        dob = dob.sort();
-        return dob;
-
-        //return $scope.studentsInfo.dateOfBirth.sort();
-
-    };
     });
