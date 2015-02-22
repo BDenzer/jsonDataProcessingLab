cfb6b4bce19d633eddc50a69ae2055858cf60ffd
xiaox232 <xiaox232@morris.umn.edu>
2015-02-21 21:42:46 -0500

    still working on search by courses

--- client/app/sortFullName/sortFullName.controller.js
+++ client/app/sortFullName/sortFullName.controller.js
@@ -113,19 +113,67 @@ angular.module('jsonDataProcessingLabApp')
       else if ($scope.majorSort == false) {
         $scope.majorSort = true;
       }
-      
+
       $scope.SearchByCourse();
 
     };
 
-    //console.log($scope.studentsInfo[1].courses[0]);
+    console.log($scope.studentsInfo[1].courses[0]);
+
+    $scope.AllCourses = function(){
+      var courseList =[];
+      var sortedCoursesList = [];
+      for (var i = 0; i < $scope.studentsInfo.length; i++) {
+        for (var j = 0; j< $scope.studentsInfo[i].courses.length;j++) {
+          if (!courseList.contains($scope.studentsInfo[i].courses[j].name)) {
+            courseList.push($scope.studentsInfo[i].courses[j].name)
+          }
+        }
+      }
+      sortedCoursesList = courseList.sort();
+      return sortedCoursesList;
+    };
+
+
+    $scope.sortedCoursesList =$scope.AllCourses();
 
     $scope.SearchByCourse = function(){
-      for (var i = 0; i < $scope.studentsInfo.length; i++) {
-        console.log($scope.studentsInfo[i].courses[0].course.name);
-        console.log("Hello");
-      }
-    }
+      var courseName = '';
+      for (var j = 0; j< $scope.sortedCoursesList.length; j++ ){
+        for (var i = 0;i < $scope.studentsInfo.length; i++) {
+          for (var c = 0; c < $scope.studentsInfo[i].courses.length; c++) {
+            if ($scope.studentsInfo[i].courses[c].contains($scope.sortedCoursesList[j])){
+              courseName = $scope.studentsInfo[i].courses[c].name;
+              return courseName;
 
+            }
+
+          }
+        }
+
+      }
+
+      };
+
+
+    $scope.courseStatues = function(student,coursename){
+      var statues = '';
+      for (var i =0; i < student.courses.length; i++) {
+        if (student.courses[i].course.name == coursename) {
+          if (["A","B","C","D","E","F"].contains(student.courses[i].grade)){
+            statues = "Taken";
+          }
+          else {
+            statues = "Taking";
+          }
+        }
+      }
+      return statues;
+    };
+
+
+    //var options = {
+    //  valueNames : ['courseName']
+    //}
 
 });
