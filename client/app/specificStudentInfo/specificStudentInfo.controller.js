c18790e1ba320c12d42dfcb06e65be20521588dc
Rorschach22 <mcart046@morris.umn.edu>
2015-02-22 20:24:41 -0600

    deleted unused code

--- client/app/specificStudentInfo/specificStudentInfo.controller.js
+++ client/app/specificStudentInfo/specificStudentInfo.controller.js
@@ -13,24 +13,4 @@ angular.module('jsonDataProcessingLabApp')
     socket.syncUpdates('student', $scope.studentsInfo);
   });
 
-
-
-      $scope.showdetails = function(student){
-        document.getElementById("coursesInfo").innerHTML = "";
-        //document.getElementById("studentInfo").innerHTML = "";
-        document.getElementById("coursesInfo").innerHTML =  student.firstName + " "+ student.lastName + "<br/>"
-                                                            + "Date of Birth: " +  student.dateOfBirth + "<br/>"
-                                                            + "Gender: " +  student.gender + "<br/>"
-                                                            + "Email: " +  student.email + "<br/>"
-                                                            + "Phone: " +  student.phone + "<br/>"
-                                                            + "Address: " +  student.address + "<br/>";
-      };
-
-    $scope.showCourses = function(student){
-      document.getElementById("coursesInfo").innerHTML = "";
-      for(var i = 0; i < student.courses.length; i++) {
-        document.getElementById("coursesInfo").innerHTML =  document.getElementById("coursesInfo").innerHTML +  "Course Name: " + student.courses[i].course.name + "       Grade: " + student.courses[i].grade + "<br/>";
-      }
-      document.getElementById("coursesInfo").innerHTML =  student.firstName + " "+ student.lastName + "<br/>" +  document.getElementById("coursesInfo").innerHTML
-    };
   });
